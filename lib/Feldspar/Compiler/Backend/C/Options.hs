51eb348d5c7a127b8bbdbdb445aefbc6afd5b6d7
Emil Axelsson <emax@chalmers.se>
2012-09-25 16:31:52 +0200

    Removing overlapping instance `Show (... -> ...)`

--- lib/Feldspar/Compiler/Backend/C/Options.hs
+++ lib/Feldspar/Compiler/Backend/C/Options.hs
@@ -1,11 +1,11 @@
 --
 -- Copyright (c) 2009-2011, ERICSSON AB
 -- All rights reserved.
--- 
+--
 -- Redistribution and use in source and binary forms, with or without
 -- modification, are permitted provided that the following conditions are met:
--- 
---     * Redistributions of source code must retain the above copyright notice, 
+--
+--     * Redistributions of source code must retain the above copyright notice,
 --       this list of conditions and the following disclaimer.
 --     * Redistributions in binary form must reproduce the above copyright
 --       notice, this list of conditions and the following disclaimer in the
@@ -13,10 +13,10 @@
 --     * Neither the name of the ERICSSON AB nor the names of its contributors
 --       may be used to endorse or promote products derived from this software
 --       without specific prior written permission.
--- 
+--
 -- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 -- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
--- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 -- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
 -- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
 -- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
@@ -31,6 +31,7 @@
 module Feldspar.Compiler.Backend.C.Options where
 
 import Data.Typeable
+import Text.Show.Functions
 
 import Feldspar.Compiler.Imperative.Representation
 import Feldspar.Compiler.Imperative.Frontend hiding (UserType, Type, Var)
@@ -64,9 +65,6 @@ type ShowValue = Constant () -> String
 instance Eq ShowValue where
     (==) _ _ = True
 
-instance Show ShowValue where
-    show _ = "<<ShowValue>>"
-
 data IsRestrict = Restrict | NoRestrict
     deriving (Show,Eq)
 
@@ -84,7 +82,7 @@ data Rule where
 
 instance Show Rule where
     show _ = "Transformation rule."
-    
+
 instance Eq Rule where
     _ == _ = False
 
