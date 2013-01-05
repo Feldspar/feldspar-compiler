6c31238307ba35ce37d2c51fe58c6527dc005808
Anders Persson <anders.cj.persson@gmail.com>
2013-01-06 00:53:31 +0100

    Disable the ConstantFolding plugin

--- lib/Feldspar/Compiler/Compiler.hs
+++ lib/Feldspar/Compiler/Compiler.hs
@@ -158,7 +158,7 @@ pluginChain externalInfo
     = executePlugin RulePlugin (ruleExternalInfo externalInfo)
 --    . executePlugin TypeDefinitionGenerator (typeDefinitionGeneratorExternalInfo externalInfo)
 --    . executePlugin ConstantFolding ()
-    . executePlugin ConstantFolding ()
+--    . executePlugin ConstantFolding ()
     . executePlugin Precompilation (precompilationExternalInfo externalInfo)
     . executePlugin RulePlugin (primitivesExternalInfo externalInfo)
 --    . executePlugin Free ()
