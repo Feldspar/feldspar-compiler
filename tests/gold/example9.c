32dabfd0bd5963e6aeda4e7947c6e39bc16beeb2
Anders Persson <anders.cj.persson@gmail.com>
2013-01-11 18:03:57 +0100

    Update testcase for new code generation

--- tests/gold/example9.c
+++ tests/gold/example9.c
@@ -11,18 +11,17 @@
 #include <complex.h>
 
 
-void example9(int32_t v0, int32_t * out)
+void example9(int32_t v0, int32_t *out)
 {
-    int32_t v1;
-
-    v1 = (v0 + 20);
-    if((v0 < 5))
-    {
-        (* out) = (v1 * 3);
-    }
-    else
-    {
-        (* out) = (v1 * 30);
-    }
+  int32_t v1;
+  
+  v1 = (v0 + 20);
+  if ((v0 < 5))
+  {
+    *out = (v1 * 3);
+  }
+  else
+  {
+    *out = (v1 * 30);
+  }
 }
-
