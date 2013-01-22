ab61597fc81d1983682675e97389f737d33218f2
Anders Persson <anders.cj.persson@gmail.com>
2013-01-22 10:04:13 +0100

    Add framework for buildable tests
    Use `mkBuildTest` to create a testcase that will compile a function to C
    code and further compile the C code on the host to check that the generated
    code is sound.

--- tests/RegressionTests.hs
+++ tests/RegressionTests.hs
@@ -6,12 +6,17 @@ module Main where
 
 import Test.Framework
 import Test.Golden
+import Test.Framework.Providers.HUnit
+import Test.HUnit
 
 import qualified Prelude
 import Feldspar
 import Feldspar.Compiler
 
 import Data.Monoid ((<>))
+import Shelly
+import Data.Text.Lazy (pack)
+import Filesystem.Path.CurrentOS (decodeString)
 
 example9 :: Data Int32 -> Data Int32
 example9 a = condition (a<5) (3*(a+20)) (30*(a+20))
@@ -29,10 +34,22 @@ writeGoldFile fun name opts = compile fun ("tests/gold/" <> name) name opts
 mkGoldTest fun name opts = goldenVsFile name ("tests/gold/" <> name <> ".c") ("tests/" <> name <> ".c")
                          $ compile fun ("tests/" <> name) name opts
 
+ghc = command_ (decodeString "ghc") [ pack "-c"
+                                    , pack "-package feldspar-compiler"
+                                    , pack "-Wall"
+                                    ]
+
+mkBuildTest fun name opts = testCase name $ do let base  = "tests/" <> name <> "_build_test"
+                                                   cfile = base <> ".c"
+                                               compile fun base name opts
+                                               shellyNoDir $ ghc [pack cfile]
+
 tests = testGroup "RegressionTests" 
     [ mkGoldTest example9 "example9" defaultOptions
     , mkGoldTest topLevelConsts "topLevelConsts" defaultOptions
     , mkGoldTest topLevelConsts "topLevelConsts_native" nativeOpts
+    , mkBuildTest topLevelConsts "topLevelConsts" defaultOptions
+    , mkBuildTest topLevelConsts "topLevelConsts_native" nativeOpts
     ]
 
 main = defaultMain [tests]
