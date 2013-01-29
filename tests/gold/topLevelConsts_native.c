69f99d4639646a05491ff94baede75a616bf1c16
Anders Persson <anders.cj.persson@gmail.com>
2013-01-29 16:02:52 +0100

    Update test cases

--- tests/gold/topLevelConsts_native.c
+++ tests/gold/topLevelConsts_native.c
@@ -11,14 +11,14 @@
 #include <complex.h>
 
 
-void topLevelConsts__native(uint32_t v0, uint32_t v1, uint32_t *out)
+void topLevelConsts__native(uint32_t *v0, uint32_t *v1, uint32_t *out)
 {
   uint32_t v2;
   uint32_t x0[5];
   uint32_t x1[5];
   
-  v2 = (v1 + 5);
-  if ((v0 < 5))
+  v2 = (*v1 + 5);
+  if ((*v0 < 5))
   {
     assert(&x0);
     x0[0] = 2;
