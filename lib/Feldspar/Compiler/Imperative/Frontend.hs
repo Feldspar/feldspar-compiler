e8c733f01e32035984daca1ef75771042521818f
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2013-02-02 14:49:38 +0100

    Put the logic relating to freeing memory in the declaration instead of relying on magic types.

--- lib/Feldspar/Compiler/Imperative/Frontend.hs
+++ lib/Feldspar/Compiler/Imperative/Frontend.hs
@@ -86,9 +86,7 @@ initArray arr len = call "initArray" [Out arr, In s, In len]
     go _               = error $ "Feldspar.Compiler.Imperative.Frontend.initArray: invalid type of array " ++ show arr ++ "::" ++ show (typeof arr)
 
 freeArray :: Variable () -> Program ()
-freeArray arr
-  | Pointer{} <- typeof arr = Empty -- Pointers are aliases in practice
-  | otherwise = call "freeArray" [Out $ AddrOf $ varToExpr arr]
+freeArray arr = call "freeArray" [Out $ AddrOf $ varToExpr arr]
 
 freeArrays :: [Declaration ()] -> [Program ()]
 freeArrays defs = map freeArray arrays
