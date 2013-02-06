2bbc16446403d3ea58f2e8b1b82c413e6cc5434e
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2013-02-06 14:31:46 +0100

    Update expected test output for latest -language.

--- tests/gold/metrics.c
+++ tests/gold/metrics.c
@@ -14,34 +14,35 @@
 void metrics(struct array * v0, struct array * v1, struct array * v2, struct array * * out)
 {
   uint32_t v20;
-  uint32_t len0;
-  struct array * st1 = calloc(1, sizeof(struct array));
+  uint32_t v21;
+  struct array * st0 = calloc(1, sizeof(struct array));
   struct array * * v17 = NULL;
-  uint32_t len2;
+  uint32_t len1;
   
-  v20 = getLength(v0);
-  len0 = getLength(v2);
-  initArray(st1, sizeof(int32_t), 8);
-  at(int32_t,st1,0) = -32678;
-  at(int32_t,st1,1) = -32678;
-  at(int32_t,st1,2) = -32678;
-  at(int32_t,st1,3) = -32678;
-  at(int32_t,st1,4) = -32678;
-  at(int32_t,st1,5) = -32678;
-  at(int32_t,st1,6) = -32678;
-  at(int32_t,st1,7) = -32678;
-  *v17 = st1;
-  initArray(*out, (0 - sizeof(struct array *)), len0);
-  for (uint32_t v16 = 0; v16 < len0; v16 += 1)
+  v20 = getLength(v2);
+  v21 = getLength(v0);
+  initArray(st0, sizeof(int32_t), 8);
+  at(int32_t,st0,0) = -32678;
+  at(int32_t,st0,1) = -32678;
+  at(int32_t,st0,2) = -32678;
+  at(int32_t,st0,3) = -32678;
+  at(int32_t,st0,4) = -32678;
+  at(int32_t,st0,5) = -32678;
+  at(int32_t,st0,6) = -32678;
+  at(int32_t,st0,7) = -32678;
+  *v17 = st0;
+  initArray(*out, (0 - sizeof(struct array *)), v20);
+  for (uint32_t v16 = 0; v16 < v20; v16 += 1)
   {
-    len2 = min(getLength(at(struct array *,v2,v16)), v20);
-    initArray(at(struct array *,*out,v16), sizeof(int32_t), len2);
-    for (uint32_t v18 = 0; v18 < len2; v18 += 1)
+    len1 = min(getLength(at(struct array *,v2,v16)), v21);
+    initArray(at(struct array *,*out,v16), sizeof(int32_t), len1);
+    for (uint32_t v18 = 0; v18 < len1; v18 += 1)
     {
       at(int32_t,at(struct array *,*out,v16),v18) = at(int32_t,*v17,(at(struct s_unsignedS32_unsignedS32,at(struct array *,v2,v16),v18)).member1);
     }
     *v17 = at(struct array *,*out,v16);
   }
-  freeArray(st1);
+  setLength(*out, v20);
+  freeArray(st0);
   freeArray(*v17);
 }
