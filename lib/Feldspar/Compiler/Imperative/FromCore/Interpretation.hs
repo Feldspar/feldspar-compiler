0951411fd8aa0104489bca1778a10a3aab22de62
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2013-10-05 14:43:48 +0200

    Remove a spurious BlockProgram constructor.

--- lib/Feldspar/Compiler/Imperative/FromCore/Interpretation.hs
+++ lib/Feldspar/Compiler/Imperative/FromCore/Interpretation.hs
@@ -335,7 +335,7 @@ tellDef :: [Entity ()] -> CodeWriter ()
 tellDef es = tell $ mempty {def = es}
 
 tellProg :: [Program ()] -> CodeWriter ()
-tellProg [BlockProgram (Block [] ps)] = tell $ mempty {block = toBlock $ Sequence [ps]}
+tellProg [BlockProgram b@(Block [] _)] = tell $ mempty {block = b}
 tellProg ps = tell $ mempty {block = toBlock $ Sequence ps}
 
 tellDeclWith :: Bool -> [Declaration ()] -> CodeWriter ()
