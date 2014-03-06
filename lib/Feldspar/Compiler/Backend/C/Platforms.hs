e76ca064d666592b92cb5b7a992f15798b65c85a
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-03-06 18:35:45 +0100

    Avoid silently filtering out some malformed calls to copy.

--- lib/Feldspar/Compiler/Backend/C/Platforms.hs
+++ lib/Feldspar/Compiler/Backend/C/Platforms.hs
@@ -137,12 +137,12 @@ showConstant c               = show c
 arrayRules :: [Rule]
 arrayRules = [rule copy]
   where
-    copy (ProcedureCall "copy" ps) = [replaceWith $ toProgram $ deepCopy [e | ValueParameter e <- ps]]
+    copy (ProcedureCall "copy" ps) = [replaceWith $ toProgram $ deepCopy ps]
     copy _ = []
     toProgram ss = if null ss then Empty else Sequence ss
 
-deepCopy :: [Expression ()] -> [Program ()]
-deepCopy [arg1, arg2]
+deepCopy :: [ActualParameter ()] -> [Program ()]
+deepCopy [ValueParameter arg1, ValueParameter arg2]
   | arg1 == arg2 
   = []
 
@@ -155,17 +155,17 @@ deepCopy [arg1, arg2]
   = initArray (Just arg1) l:map (\i -> Assign (ArrayElem arg1 (litI32 i)) (ArrayElem arg2 (litI32 i))) [0..(n-1)]
 
   | StructType name fts <- typeof arg2
-  = concatMap (\ (fieldName,_) -> deepCopy [StructField arg1 fieldName, StructField arg2 fieldName]) fts
+  = concatMap (\ (fieldName,_) -> deepCopy [ValueParameter $ StructField arg1 fieldName, ValueParameter $ StructField arg2 fieldName]) fts
 
   | not (isArray (typeof arg1))
   = [Assign arg1 arg2]
 
-deepCopy (arg1 : ins'@(in1:ins))
+deepCopy (ValueParameter arg1 : ins'@(ValueParameter in1:ins))
   | isArray (typeof arg1)
   = [ initArray (Just arg1) expDstLen, copyFirstSegment ] ++ flattenCopy (ValueParameter arg1) ins argnLens arg1len
     where expDstLen = foldr ePlus (litI32 0) aLens
           copyFirstSegment = if arg1 == in1 then Empty else call "copyArray" [ValueParameter arg1, ValueParameter in1]
-          aLens@(arg1len:argnLens) = map arrayLength ins'
+          aLens@(arg1len:argnLens) = map (\(ValueParameter src) -> arrayLength src) ins'
 
 deepCopy _ = error "Multiple scalar arguments to copy"
 
@@ -201,9 +201,9 @@ nativeArrayRules = [rule toNativeExpr, rule toNativeProg, rule toNativeVariable]
                         then Just $ upperBound r
                         else Nothing
 
-flattenCopy :: ActualParameter () -> [Expression ()] -> [Expression ()] -> Expression () -> [Program ()]
+flattenCopy :: ActualParameter () -> [ActualParameter ()] -> [Expression ()] -> Expression () -> [Program ()]
 flattenCopy _ [] [] _ = []
-flattenCopy dst (t:ts) (l:ls) cLen = call "copyArrayPos" [dst, ValueParameter cLen, ValueParameter t]
+flattenCopy dst (t:ts) (l:ls) cLen = call "copyArrayPos" [dst, ValueParameter cLen, t]
                                    : flattenCopy dst ts ls (ePlus cLen l)
 
 ePlus :: Expression () -> Expression () -> Expression ()
