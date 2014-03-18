a5e8e99488e7b66e304b3d28d6e0e9bde152c7c3
Anders Persson <anders.cj.persson@gmail.com>
2014-03-18 10:21:42 +0100

    Update tests and benchmarks to multidim vectors

--- benchs/CRC.hs
+++ benchs/CRC.hs
@@ -12,8 +12,11 @@ import Control.Exception (evaluate)
 
 import Criterion.Main
 
+len :: Length
+len = 16 * 1024
+
 testdata :: [Word8]
-testdata = Prelude.take (16*1024) $ cycle [1,2,3,4]
+testdata = Prelude.take (fromIntegral len) $ cycle [1,2,3,4]
 
 naive :: Vector1 Word8 -> Data Word16
 naive = crcNaive 0x8005 0
@@ -21,30 +24,30 @@ naive = crcNaive 0x8005 0
 normal :: Vector1 Word8 -> Data Word16
 normal v = share (makeCrcTable 0x8005) $ \t -> crcNormal t 0 v
 
-h_naive :: [Word8] -> Word16
+h_naive :: ([Length],[Word8]) -> Word16
 h_naive = eval naive
 loadFun 'naive
 
-h_normal :: [Word8] -> Word16
+h_normal :: ([Length],[Word8]) -> Word16
 h_normal = eval normal
 loadFun 'normal
 
 main :: IO ()
 main = with def $ \out -> do
     d  <- evaluate $ force testdata
-    pd <- pack d >>= evaluate
+    pd <- pack ([len],d) >>= evaluate
     _  <- evaluate c_naive_builder
     _  <- evaluate c_normal_builder
     defaultMain
       [
         bgroup "evaluated"
-          [ bench "h_naive"  $ nf h_naive  (Prelude.take 1024 d)
-          , bench "h_normal" $ nf h_normal (Prelude.take 1024 d)
+          [ bench "h_naive"  $ nf h_naive  ([1024],Prelude.take 1024 d)
+          , bench "h_normal" $ nf h_normal ([1024],Prelude.take 1024 d)
           ]
       , bgroup "compiled"
           [ bgroup "marshal"
-              [ bench "c_naive"  $ c_naive_worker d
-              , bench "c_normal" $ c_normal_worker d
+              [ bench "c_naive"  $ c_naive_worker ([len],d)
+              , bench "c_normal" $ c_normal_worker ([len],d)
               ]
           , bgroup "raw"
               [ bench "c_naive"  $ c_naive_raw pd out
--- tests/CallingConvention.hs
+++ tests/CallingConvention.hs
@@ -16,23 +16,30 @@ import Feldspar.Vector
 import Feldspar.Compiler
 import Feldspar.Compiler.Plugin
 
+import Control.Applicative
+
+withVector1D :: Int -> Gen a -> Gen ([WordN],[a])
+withVector1D l ga = do
+    xs <- vectorOf l ga
+    return ([Prelude.fromIntegral l],xs)
+
 pairArg :: (Data Word8,Data IntN) -> Data IntN
 pairArg (a,b) = i2n a + b
 
 pairRes :: Data Word16 -> (Data WordN, Data IntN)
 pairRes a = (i2n a, i2n a)
 
-vecId :: Vector1 Word32 -> Vector1 Word32
+vecId :: Pull1 Word32 -> Pull1 Word32
 vecId = id
 
-vectorInPair :: (Vector1 WordN, Data WordN) -> (Data WordN, Vector1 WordN)
+vectorInPair :: (Pull1 WordN, Data WordN) -> (Data WordN, Pull1 WordN)
 vectorInPair (v,a) = (a,v)
 
-vectorInVector :: Vector (Vector1 WordN) -> Data WordN
-vectorInVector v = sum $ map sum v
+vectorInVector :: Pull DIM1 (Pull1 WordN) -> Data WordN
+vectorInVector v = fromZero $ sum $ map (fromZero . sum) v
 
-vectorInPairInVector :: Data WordN -> Vector (Data WordN, Vector1 WordN)
-vectorInPairInVector l = indexed l $ \i -> (i, indexed i id)
+vectorInPairInVector :: Data WordN -> Pull DIM1 (Data WordN, Pull1 WordN)
+vectorInPairInVector l = indexed1 l $ \i -> (i, indexed1 i id)
 
 loadFun 'pairArg
 loadFun 'pairRes
@@ -43,13 +50,21 @@ loadFun 'vectorInPairInVector
 
 prop_pairArg      = eval pairArg === c_pairArg
 prop_pairRes      = eval pairRes === c_pairRes
-prop_vecId        = eval vecId   === c_vecId
-prop_vectorInPair = eval vectorInPair === c_vectorInPair
-prop_vectorInVector = eval vectorInVector === c_vectorInVector
-prop_vectorInPairInVector = forAll (choose (0,63)) $ \l -> eval vectorInPairInVector l === c_vectorInPairInVector l
+prop_vecId        = sized $ \l -> do
+                       xs <- withVector1D l arbitrary
+                       eval vecId xs === c_vecId xs
+prop_vectorInPair = sized $ \l -> do
+                      p <- (,) <$> withVector1D l arbitrary <*> arbitrary
+                      eval vectorInPair p === c_vectorInPair p
+prop_vectorInVector = sized $ \l1 -> do
+                        sized $ \l2 -> do
+                          v <- withVector1D l1 (withVector1D l2 arbitrary)
+                          eval vectorInVector v === c_vectorInVector v
+prop_vectorInPairInVector = forAll (choose (0,63)) $ \l ->
+                              eval vectorInPairInVector l === c_vectorInPairInVector l
 
 tests :: TestTree
-tests = testGroup "CodeGeneration"
+tests = testGroup "CallingConvention"
     [ testProperty "pairArg" prop_pairArg
     , testProperty "pairRes" prop_pairRes
     , testProperty "vecId"   prop_vecId
