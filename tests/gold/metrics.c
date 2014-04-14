c33a8d884bbd39b16975f36836e9727c62d75d06
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-04-15 00:14:11 +0200

    Update test outputs from frontend changes.

--- tests/gold/metrics.c
+++ tests/gold/metrics.c
@@ -5,36 +5,33 @@ void metrics(struct s_2_arr_unsignedS32_arr_signedS32 * v0, struct s_2_arr_unsig
 {
   uint32_t v66;
   uint32_t v65;
-  struct array * e0 = NULL;
   struct array * v71 = NULL;
   uint32_t v67;
-  uint32_t len1;
-  struct s_2_arr_unsignedS32_arr_signedS32 st2 = { .member1 = NULL, .member2 = NULL };
+  uint32_t len0;
+  struct s_2_arr_unsignedS32_arr_signedS32 st1 = { .member1 = NULL, .member2 = NULL };
   struct s_2_arr_unsignedS32_arr_signedS32 * v15 = NULL;
   uint32_t v68;
   struct s_2_arr_unsignedS32_arr_s_2_unsignedS32_unsignedS32 v69 = { .member1 = NULL, .member2 = NULL };
   uint32_t v72;
   
   v65 = at(uint32_t,(*v2).member1,0);
-  e0 = setLength(e0, sizeof(uint32_t), 1);
-  at(uint32_t,e0,0) = v65;
-  v66 = at(uint32_t,e0,0);
+  v66 = v65;
   v67 = at(uint32_t,(*v0).member1,0);
-  len1 = at(uint32_t,(*v2).member1,0);
-  (st2).member1 = initArray((st2).member1, sizeof(uint32_t), 1);
-  at(uint32_t,(st2).member1,0) = 8;
-  (st2).member2 = initArray((st2).member2, sizeof(int32_t), 8);
-  at(int32_t,(st2).member2,0) = -32678;
-  at(int32_t,(st2).member2,1) = -32678;
-  at(int32_t,(st2).member2,2) = -32678;
-  at(int32_t,(st2).member2,3) = -32678;
-  at(int32_t,(st2).member2,4) = -32678;
-  at(int32_t,(st2).member2,5) = -32678;
-  at(int32_t,(st2).member2,6) = -32678;
-  at(int32_t,(st2).member2,7) = -32678;
-  v15 = &st2;
-  v71 = initArray(v71, sizeof(struct s_2_arr_unsignedS32_arr_signedS32), len1);
-  for (uint32_t v14 = 0; v14 < len1; v14 += 1)
+  len0 = at(uint32_t,(*v2).member1,0);
+  (st1).member1 = initArray((st1).member1, sizeof(uint32_t), 1);
+  at(uint32_t,(st1).member1,0) = 8;
+  (st1).member2 = initArray((st1).member2, sizeof(int32_t), 8);
+  at(int32_t,(st1).member2,0) = -32678;
+  at(int32_t,(st1).member2,1) = -32678;
+  at(int32_t,(st1).member2,2) = -32678;
+  at(int32_t,(st1).member2,3) = -32678;
+  at(int32_t,(st1).member2,4) = -32678;
+  at(int32_t,(st1).member2,5) = -32678;
+  at(int32_t,(st1).member2,6) = -32678;
+  at(int32_t,(st1).member2,7) = -32678;
+  v15 = &st1;
+  v71 = initArray(v71, sizeof(struct s_2_arr_unsignedS32_arr_signedS32), len0);
+  for (uint32_t v14 = 0; v14 < len0; v14 += 1)
   {
     v68 = min(at(uint32_t,(at(struct s_2_arr_unsignedS32_arr_s_2_unsignedS32_unsignedS32,(*v2).member2,v14)).member1,0), v67);
     (v69).member1 = initArray((v69).member1, sizeof(uint32_t), getLength((at(struct s_2_arr_unsignedS32_arr_s_2_unsignedS32_unsignedS32,(*v2).member2,v14)).member1));
@@ -62,6 +59,5 @@ void metrics(struct s_2_arr_unsignedS32_arr_signedS32 * v0, struct s_2_arr_unsig
     copyArray((at(struct s_2_arr_unsignedS32_arr_signedS32,(*out).member2,v9)).member2, (at(struct s_2_arr_unsignedS32_arr_signedS32,v71,v9)).member2);
     (at(struct s_2_arr_unsignedS32_arr_signedS32,(*out).member2,v9)).member2 = setLength((at(struct s_2_arr_unsignedS32_arr_signedS32,(*out).member2,v9)).member2, sizeof(int32_t), v72);
   }
-  freeArray(e0);
   freeArray(v71);
 }
