1a4b372d8b4671bd0f2015f1a226a7a4770940f3
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-08-13 21:20:07 +0200

    HLint fixes.

--- benchs/BenchmarkUtils.hs
+++ benchs/BenchmarkUtils.hs
@@ -4,16 +4,16 @@ import Feldspar.Compiler.Plugin (pack)
 import Control.Exception (evaluate)
 import Criterion.Main (bench)
 import Criterion.Config (Config(..),defaultConfig,ljust)
-import Data.List (intersperse)
+import Data.List (intercalate)
 
 mkConfig report = defaultConfig { cfgPerformGC = ljust True
                                 , cfgReport    = ljust report
                                 }
 
-dimToString ls = concat $ intersperse "x" (map show ls)
+dimToString ls = intercalate "x" (map show ls)
 
 mkData ds ls = do putStrLn $ unwords ["Alloc array with", dimToString ls, "elements"]
                   evaluate =<< pack (ls, take (fromIntegral $ product ls) ds)
 
-mkBench name ls fun = do
+mkBench name ls fun =
     return $ bench (name ++ "_" ++ dimToString ls) fun
