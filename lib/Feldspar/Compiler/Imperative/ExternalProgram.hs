6ca1c1a97c55149ec7615ea1c0476b855868d08e
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2015-01-21 12:07:40 +0100

    Improve type inference some more for assignments in ExternalProgram.

--- lib/Feldspar/Compiler/Imperative/ExternalProgram.hs
+++ lib/Feldspar/Compiler/Imperative/ExternalProgram.hs
@@ -251,6 +251,12 @@ impureExpToProgram env (Assign e@Var{} JustAssign
                                f@(FnCall (Var (Id "at" _) _) [e1, e2] _) _)
   = (env', R.Assign (Just $ expToExpression env e) (expToExpression env' f))
    where env' = fixupEnv env e1 $ varType (varToVariable env e)
+impureExpToProgram env (Assign e@(FnCall (Var (Id "at" _) _) [e1, e2] _) JustAssign
+                               f@(FnCall (Var (Id "at" _) _) [e1', e2'] _) _)
+  = (env'', R.Assign (Just $ expToExpression env'' e) (expToExpression env'' f))
+   -- Hope LHS or RHS has proper types. Propagate to the other side.
+   where env'' = fixupEnv env' e1 $ typeof (expToExpression env' f)
+         env'  = fixupEnv env e1' $ typeof (expToExpression env e)
 impureExpToProgram env (Assign e1 JustAssign e2 _)
   = (env, R.Assign (Just $ expToExpression env e1) (expToExpression env e2))
 impureExpToProgram _ e = error ("impureExpToProgram: " ++ show e)
@@ -528,6 +534,13 @@ plusEnv (TPEnv vs1 tdefs1 hdefs) (TPEnv vs2 tdefs2 _ )
 
 -- Patch the type information in the environment when we learn more.
 fixupEnv :: TPEnv -> Exp -> R.Type -> TPEnv
+fixupEnv env _ VoidType = env -- No new type information.
+fixupEnv env (UnOp Deref (Var (Id s _) _) _) tp = env { vars = goVar (vars env) }
+  where goVar [] = []
+        goVar (p@(n, Variable (MachineVector l (Pointer (ArrayType r _))) n'):t)
+         | s == n = (n, Variable (MachineVector l (Pointer (ArrayType r tp))) n'):goVar t
+        goVar (p:t) = p:goVar t
+fixupEnv _ (UnOp Deref e _) _ = error ("fixupEnv: No support for " ++ show e)
 fixupEnv env (Var (Id s _) _) tp = env { vars = goVar (vars env) }
   where goVar [] = []
         goVar (p@(n, Variable (ArrayType r _) n'):t)
