1db77e1149ec8b258078a349f932ef62ec98067b
Emil Axelsson <emax@chalmers.se>
2015-09-22 17:40:22 +0200

    Added a method for retreiving the path to the C include files

--- lib/Feldspar/Compiler.hs
+++ lib/Feldspar/Compiler.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 
 module Feldspar.Compiler
     ( compile
@@ -16,8 +17,38 @@ module Feldspar.Compiler
     , c99PlatformOptions
     , c99OpenMpPlatformOptions
     , tic64xPlatformOptions
+    , feldsparCIncludes
     ) where
+
+import System.FilePath
 
 import Feldspar.Compiler.Internal
 import Feldspar.Core.Interpretation (FeldOpts(..), Target(..))
+
+#ifdef CABAL_IS_USED
+
+import Paths_feldspar_compiler
+
+-- | Get the path to the C include files installed by Cabal
+feldsparCIncludes :: IO FilePath
+feldsparCIncludes = fmap (</> "include") getLibDir
+
+#else
+
+import System.Directory
+
+-- | Get the path to the C include files
+--
+-- It returns a platform-independent representation of the directory
+-- @../feldspar-compiler/lib/Feldspar/C@. This assumes that the method is called
+-- either from the root of the @feldspar-compiler@ repository, or from a
+-- different directory located next to @feldspar-compiler@.
+feldsparCIncludes :: IO FilePath
+feldsparCIncludes = do
+    dir <- getCurrentDirectory
+    return $ case splitPath dir of
+        [] -> dir
+        ds -> joinPath $ init ds ++ ["feldspar-compiler","lib","Feldspar","C"]
+
+#endif
 
