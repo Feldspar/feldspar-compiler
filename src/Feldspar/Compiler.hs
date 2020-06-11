a8216510656bf821f38256278f8963efd51a3554
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2020-06-11 12:39:00 +0200

    Remove unused feldsparCIncludes

--- src/Feldspar/Compiler.hs
+++ src/Feldspar/Compiler.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE CPP #-}
-
 module Feldspar.Compiler
     ( compile
     , compileUT
@@ -21,38 +19,8 @@ module Feldspar.Compiler
     , c99PlatformOptions
     , c99OpenMpPlatformOptions
     , tic64xPlatformOptions
-    , feldsparCIncludes
     ) where
-
-import System.FilePath
 
 import Feldspar.Compiler.Internal
 import Feldspar.Core.Interpretation (FeldOpts(..), Target(..))
-
-#ifdef CABAL_IS_USED
-
-import Paths_feldspar_compiler
-
--- | Get the path to the C include files installed by Cabal
-feldsparCIncludes :: IO FilePath
-feldsparCIncludes = fmap (</> "include") getLibDir
-
-#else
-
-import System.Directory
-
--- | Get the path to the C include files
---
--- It returns a platform-independent representation of the directory
--- @../feldspar-compiler/lib/Feldspar/C@. This assumes that the method is called
--- either from the root of the @feldspar-compiler@ repository, or from a
--- different directory located next to @feldspar-compiler@.
-feldsparCIncludes :: IO FilePath
-feldsparCIncludes = do
-    dir <- getCurrentDirectory
-    return $ case splitPath dir of
-        [] -> dir
-        ds -> joinPath $ init ds ++ ["feldspar-compiler","clib"]
-
-#endif
 
